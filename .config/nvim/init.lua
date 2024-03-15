0c788bac01c83aac891f5015922803529d19d2ea
sei-12 <sei.t11@icloud.com>
2024-03-15 09:25:26 +0900

    init.vim -> init.lua

---
+++ .config/nvim/init.lua
@@ -0,0 +1,14 @@
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.relativenumber = true
+
+vim.api.nvim_set_keymap('n','#','*Nciw',{noremap = true})
+vim.api.nvim_set_keymap('n','<space>','i<space><ESC>',{noremap = true})
+
+local Plug = vim.fn['plug#']
+vim.call('plug#begin')
+Plug 'tpope/vim-surround'
+Plug 'tpope/vim-repeat'
+Plug 'tpope/vim-commentary'
+Plug 'junegunn/fzf'
+Plug 'junegunn/fzf.vim'
+vim.call('plug#end')
