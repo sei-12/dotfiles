346e7ba64fc9cdf1b157fc82f3d3b1692241dc7b
sei-12 <sei.t11@icloud.com>
2024-09-16 17:20:19 +0900

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,42 +2,45 @@ vim.opt.clipboard = 'unnamedplus'
 vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 
-vim.api.nvim_set_keymap('n','#','*Nciw',{noremap = true})
-vim.api.nvim_set_keymap('n','<space>','i<space><ESC>l',{noremap = true})
-
-local Plug = vim.fn['plug#']
-vim.call('plug#begin')
-Plug 'tpope/vim-surround'
-Plug 'tpope/vim-repeat'
-Plug 'tpope/vim-commentary'
-Plug 'junegunn/fzf'
-Plug 'junegunn/fzf.vim'
-Plug 'nvim-tree/nvim-tree.lua'
-Plug 'nvim-tree/nvim-web-devicons'
-vim.call('plug#end')
-
--- disable netrw at the very start of your init.lua
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
--- optionally enable 24-bit colour
 vim.opt.termguicolors = true
+vim.cmd('highlight Normal guibg=none')
 
--- empty setup using defaults
-require("nvim-tree").setup()
+-- vim.api.nvim_set_keymap('n','#','*Nciw',{noremap = true})
+-- vim.api.nvim_set_keymap('n','<space>','i<space><ESC>l',{noremap = true})
 
--- OR setup with some options
-require("nvim-tree").setup({
-  sort = {
-    sorter = "case_sensitive",
-  },
-  view = {
-    width = 30,
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
-})
+-- local Plug = vim.fn['plug#']
+-- vim.call('plug#begin')
+-- Plug 'tpope/vim-surround'
+-- Plug 'tpope/vim-repeat'
+-- Plug 'tpope/vim-commentary'
+-- Plug 'junegunn/fzf'
+-- Plug 'junegunn/fzf.vim'
+-- Plug 'nvim-tree/nvim-tree.lua'
+-- Plug 'nvim-tree/nvim-web-devicons'
+-- vim.call('plug#end')
+
+-- -- disable netrw at the very start of your init.lua
+-- vim.g.loaded_netrw = 1
+-- vim.g.loaded_netrwPlugin = 1
+
+-- -- optionally enable 24-bit colour
+-- vim.opt.termguicolors = true
+
+-- -- empty setup using defaults
+-- require("nvim-tree").setup()
+
+-- -- OR setup with some options
+-- require("nvim-tree").setup({
+--   sort = {
+--     sorter = "case_sensitive",
+--   },
+--   view = {
+--     width = 30,
+--   },
+--   renderer = {
+--     group_empty = true,
+--   },
+--   filters = {
+--     dotfiles = true,
+--   },
+-- })
--- .zshrc
+++ .zshrc
@@ -250,3 +250,6 @@ function load-nvm(){
 	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
 	[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 }
+
+# Created by `pipx` on 2024-09-01 09:14:26
+export PATH="$PATH:/Users/sei-12/.local/bin"
