6964ac804ee853982343fe5569c35183e54d60ec
sei-12 <sei.t11@icloud.com>
2024-12-15 04:58:18 +0900

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -12,4 +12,61 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 
+require("lazy").setup({
+    "tpope/vim-surround",
+    {
+      "folke/tokyonight.nvim",
+      lazy = false,
+      priority = 1000,
+      opts = {
+	      style = "night",
+	      transparent     = true, 
+	      terminal_colors = true,
+      },
+    },
+    {
+      'smoka7/hop.nvim',
+      version = "*",
+      opts = {
+          keys = 'flisuetovxqpdygbzhckran'
+      }
+  },
+    "junegunn/vim-easy-align",
+    "nvim-lualine/lualine.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "junegunn/fzf",
+    "junegunn/fzf.vim",
+    "lewis6991/gitsigns.nvim",
+    "cohama/lexima.vim",
+})
+
+require('lualine').setup()
+require('gitsigns').setup()vim.opt.clipboard      = 'unnamedplus'
+
+vim.opt.relativenumber = true
+vim.opt.scrolloff      = 10
+-- indent
+vim.opt.tabstop        = 4
+vim.opt.shiftwidth     = 4
+vim.opt.smartindent    = true
+vim.opt.autoindent     = true
+vim.opt.ignorecase     = true
+
+vim.cmd('syntax enable')
+vim.cmd[[colorscheme tokyonight-storm]]
+
+vim.cmd[[
+    nmap <C-S-o> :Files<CR>
+    xmap ga <Plug>(EasyAlign)
+    nmap ga <Plug>(EasyAlign)
+    nnoremap * *N
+	nnoremap <C-f> :HopWord<CR>
+]]
+
+vim.api.nvim_create_user_command(
+	'ReloadVim',
+	function() vim.cmd("write") vim.cmd("luafile" .. vim.fn.expand('$MYVIMRC')) end,
+	{desc = 'Save current file and source $MYVIMRC'}
+)
+
 
