7123240132183b0a57c2318d249d5d5438e77158
sei-12 <sei.t11@icloud.com>
2024-12-21 07:13:52 +0900

    add plugin hop

--- .config/nvim/lua/vscode-conf.lua
+++ .config/nvim/lua/vscode-conf.lua
@@ -6,5 +6,15 @@ require("lazy").setup({
     spec = {
 		"tpope/vim-surround",
 		"kana/vim-textobj-user",
+		{
+			'smoka7/hop.nvim',
+			version = "*",
+			opts = {
+				keys = 'flisuetovxqpdygbzhckran'
+			}
+		},
     }
 })
+
+local keymap = vim.keymap
+keymap.set("n", "s", ":HopWord<CR>")
