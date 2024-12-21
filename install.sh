72c85982dd3d818a1db62d53fa33e74f2ec5fd58
sei-12 <sei.t11@icloud.com>
2024-12-21 15:11:06 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -80,3 +80,7 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 defaults write -g ApplePressAndHoldEnabled -bool false
+
+
+# VSCode Extensions
+code --install-extension asvetliakov.vscode-neovim
